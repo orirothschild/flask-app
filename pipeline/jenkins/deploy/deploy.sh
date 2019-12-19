359dc174dfee3b633181e5a260378698ac93b313
root <root@idoz.dts.local>
2019-12-19 08:40:29 -0500

    production has its job

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -1,13 +1,14 @@
 
 #!/bin/bash
 export IP=192.168.200.182
+export PASS=978645312Or
+export BUILD_TAG=10
 echo maven-project > /tmp/.auth
 
 echo $BUILD_TAG >> /tmp/.auth 
 echo $PASS >> /tmp/.auth
-echo "hello"
 scp /tmp/.auth jenkins@$IP:/tmp/.auth
-
-scp ./jenkins/deploy/publish/publish.sh jenkins@$IP:/tmp/deploy.sh
-ssh -t jenkins@192.168.200.182 "chmod +x /tmp/deploy.sh"
+scp jenkins/deploy/publish.sh jenkins@$IP:/tmp/publish.sh
+echo hello
+ssh -t jenkins@$IP "echo 1234 | sudo -S /tmp/publish.sh"
 
--- pipeline/jenkins/deploy/publish/publish.sh
+++
@@ -1,8 +0,0 @@
-!#/bin/bash
-export PASS=$(sed -n '3p' /tmp/.auth)
-export TAG=$(sed -n '2p' /tmp/.auth)
-export IMAGE=$(sed -n '1p' /tmp/.auth)
-
-docker login -u orirothschild -p $PASS
-cd ~/maven && docker-compose up -d
-
